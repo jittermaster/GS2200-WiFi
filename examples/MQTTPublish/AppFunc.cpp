ad5aa7f132e4b80e92750651251d4257a4672d6d
jittermaster <33562872+jittermaster@users.noreply.github.com>
2019-03-15 20:00:35 +0900

    MQTT Publish example

---
+++ examples/MQTTPublish/AppFunc.cpp
@@ -0,0 +1,182 @@
+/*
+ *  AppFunc.cpp - Application code of GS2200
+ *  Copyright 2019 Norikazu Goto
+ *
+ *  This work is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU Lesser General Public License as published by the Free Software Foundation; 
+ *  either version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This work is distributed in the hope that it will be useful, but without any warranty; 
+ *  without even the implied warranty of merchantability or fitness for a particular 
+ *  purpose. See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License along with 
+ *  this work; if not, write to the Free Software Foundation, 
+ *  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+
+/*-------------------------------------------------------------------------*
+ * Includes:
+ *-------------------------------------------------------------------------*/
+#include <Arduino.h>
+#include <AtCmd.h>
+#include <GS2200Hal.h>
+#include "AppFunc.h"
+
+
+/*-------------------------------------------------------------------------*
+ * Constants:
+ *-------------------------------------------------------------------------*/
+#define  SPI_MAX_BYTE 2048
+
+#define  AP_SSID     "Telit_Guest"
+#define  PASSPHRASE  "qwerty123456"
+
+#define  TCPSRVR_IP     "192.168.11.144"
+#define  TCPSRVR_PORT   "10001"
+
+
+#define  APP_DEBUG
+
+
+/*-------------------------------------------------------------------------*
+ * Types:
+ *-------------------------------------------------------------------------*/
+
+/*-------------------------------------------------------------------------*
+ * Globals:
+ *-------------------------------------------------------------------------*/
+char TCP_Data[]="GS2200 TCP Client Data Transfer.";
+
+extern uint8_t ESCBuffer[];
+extern uint32_t ESCBufferCnt;
+
+
+/*-------------------------------------------------------------------------*
+ * Locals:
+ *-------------------------------------------------------------------------*/
+
+
+/*-------------------------------------------------------------------------*
+ * Function ProtoTypes:
+ *-------------------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------------------*
+ * App_InitModule
+ *---------------------------------------------------------------------------*
+ * Description: Minimum set of node initialization AT commands
+ *---------------------------------------------------------------------------*/
+void App_InitModule(void)
+{
+	ATCMD_RESP_E r = ATCMD_RESP_UNMATCH;
+	ATCMD_REGDOMAIN_E regDomain;
+	char macid[20];
+
+	/* Try to read boot-up banner */
+	while( Get_GPIO37Status() ){
+		r = AtCmd_RecvResponse();
+		if( r == ATCMD_RESP_NORMAL_BOOT_MSG )
+			ConsoleLog("Normal Boot.\r\n");
+	}
+	
+	do {
+		r = AtCmd_AT();
+	} while (ATCMD_RESP_OK != r);
+
+
+	/* Send command to disable Echo */
+	do {
+		r = AtCmd_ATE(0);
+	} while (ATCMD_RESP_OK != r);
+
+	/* AT+WREGDOMAIN=? should run after disabling Echo, otherwise the wrong domain is obtained. */
+	do {
+		r = AtCmd_WREGDOMAIN_Q( &regDomain );
+	} while (ATCMD_RESP_OK != r);
+
+	if( regDomain != ATCMD_REGDOMAIN_TELEC ){
+		do {
+			r = AtCmd_WREGDOMAIN( ATCMD_REGDOMAIN_TELEC );
+		} while (ATCMD_RESP_OK != r);
+	}	
+
+	/* Read MAC Address */
+	do{
+		r = AtCmd_NMAC_Q( macid );
+	}while(ATCMD_RESP_OK != r); 
+
+	/* Read Version Information */
+	do {
+		r = AtCmd_VER();
+	} while (ATCMD_RESP_OK != r);
+
+
+	/* Enable Power save mode */
+	/* AT+WRXACTIVE=0, AT+WRXPS=1 */
+	do{
+		r = AtCmd_WRXACTIVE(0);
+	}while(ATCMD_RESP_OK != r); 
+
+	do{
+		r = AtCmd_WRXPS(1);
+	}while(ATCMD_RESP_OK != r); 
+
+	/* Bulk Data mode */
+	do{
+		r = AtCmd_BDATA(1);
+	}while(ATCMD_RESP_OK != r); 
+
+}
+
+
+/*---------------------------------------------------------------------------*
+ * App_ConnectAP
+ *---------------------------------------------------------------------------*
+ * Description: Associate to the AP
+ *---------------------------------------------------------------------------*/
+void App_ConnectAP(void)
+{
+	ATCMD_RESP_E r;
+
+#ifdef APP_DEBUG
+	ConsolePrintf("Associating to AP: %s\r\n", AP_SSID);
+#endif
+
+	/* Set Infrastructure mode */
+	do { 
+		r = AtCmd_WM(ATCMD_MODE_STATION);
+	}while (ATCMD_RESP_OK != r);
+
+	/* Try to disassociate if not already associated */
+	do { 
+		r = AtCmd_WD(); 
+	}while (ATCMD_RESP_OK != r);
+
+	/* Enable DHCP Client */
+	do{
+		r = AtCmd_NDHCP( 1 );
+	}while(ATCMD_RESP_OK != r); 
+
+	/* Set WPA2 Passphrase */
+	do{
+		r = AtCmd_WPAPSK( (char *)AP_SSID, (char *)PASSPHRASE );
+	}while(ATCMD_RESP_OK != r); 
+
+	/* Associate with AP */
+	do{
+		r = AtCmd_WA( (char *)AP_SSID, (char *)"", 0 );
+	}while(ATCMD_RESP_OK != r); 
+
+	/* L2 Network Status */
+	do{
+		r = AtCmd_WSTATUS();
+	}while(ATCMD_RESP_OK != r); 
+
+}
+
+
+/*-------------------------------------------------------------------------*
+ * End of File:  AppFunc.c
+ *-------------------------------------------------------------------------*/
---
+++ examples/MQTTPublish/MQTTPublish.ino
@@ -0,0 +1,118 @@
+/*
+ *  SPRESENSE_WiFi.ino - GainSpan WiFi Module Control Program
+ *  Copyright 2019 Norikazu Goto
+ *
+ *  This work is free software; you can redistribute it and/or modify it under the terms 
+ *  of the GNU Lesser General Public License as published by the Free Software Foundation; 
+ *  either version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This work is distributed in the hope that it will be useful, but without any warranty; 
+ *  without even the implied warranty of merchantability or fitness for a particular 
+ *  purpose. See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License along with 
+ *  this work; if not, write to the Free Software Foundation, 
+ *  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#include <GS2200Hal.h>
+#include <AtCmd.h>
+#include "AppFunc.h"
+
+
+#define  CONSOLE_BAUDRATE  115200
+
+#define  MQTT_SRVR     "test.mosquitto.org"
+#define  MQTT_PORT     "1883"
+#define  MQTT_CLI_ID   "Telit_Device_pub"
+#define  MQTT_TOPIC    "Telit/property"
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+	/* initialize digital pin LED_BUILTIN as an output. */
+	pinMode(LED0, OUTPUT);
+	digitalWrite( LED0, LOW );   // turn the LED off (LOW is the voltage level)
+	Serial.begin( CONSOLE_BAUDRATE ); // talk to PC
+
+	/* Initialize SPI access of GS2200 */
+	Init_GS2200_SPI();
+
+	digitalWrite( LED0, HIGH ); // turn on LED
+
+	/* Initialize AT Command Library Buffer */
+	AtCmd_Init();
+
+	/* GS2200 Association to AP */
+	App_InitModule();
+	App_ConnectAP();
+}
+
+char server_cid = 0;
+bool served = false;
+uint16_t len, count=0;
+ATCMD_MQTTparams mqttparams;
+
+// the loop function runs over and over again forever
+void loop() {
+
+	ATCMD_RESP_E resp;
+	ATCMD_NetworkStatus networkStatus;
+
+	if (!served) {
+		resp = ATCMD_RESP_UNMATCH;
+		// Start a MQTT client
+		ConsoleLog( "Start MQTT Client");
+
+		resp = AtCmd_MQTTCONNECT( &server_cid, (char *)MQTT_SRVR, (char *)MQTT_PORT, (char *)MQTT_CLI_ID );
+		if (resp != ATCMD_RESP_OK) {
+			ConsoleLog( "No Connect!" );
+			delay(2000);
+			return;
+		}
+		if (server_cid == ATCMD_INVALID_CID) {
+			ConsoleLog( "No CID!" );
+			delay(2000);
+			return;
+		}
+		
+		do {
+			resp = AtCmd_NSTAT(&networkStatus);
+		} while (ATCMD_RESP_OK != resp);
+		
+		served = true;
+	}
+	else {
+		ConsoleLog( "Start to send MQTT Message");
+		// Prepare for the next chunck of incoming data
+		WiFi_InitESCBuffer();
+		
+		// Start the loop to send the data
+		strcpy( mqttparams.topic, MQTT_TOPIC );
+		mqttparams.QoS = 0;
+		mqttparams.retain = 0;
+		while( served ){
+			sprintf( mqttparams.message, "%d", count++ );
+			mqttparams.len = strlen( mqttparams.message );
+
+			if( ATCMD_RESP_OK != AtCmd_MQTTPUBLISH( server_cid, mqttparams ) ){
+				// Data is not sent, we need to re-send the data
+				delay(10);
+			}
+
+			delay(5000); // every 5 sec 
+
+			/* just in case something from GS2200 */
+			while( Get_GPIO37Status() ){
+				resp = AtCmd_RecvResponse();
+#if 0				
+				ConsolePrintf( "GS2200 Response Status : %d\r\n", resp );
+#endif
+				if( ATCMD_RESP_DISCONNECT == resp )
+					served = false; // quite the loop
+			}
+		}
+		
+	}
+}
