63ae5dca50226c0a6551bc9eeb77958e506fdffd
shixuejun-neusoft <shixuejun@neusoft.co.jp>
2023-01-24 13:39:22 +0900

    delete TCPClient AppFunc.cpp/h

--- examples/TCPClient/TCPClient.ino
+++ examples/TCPClient/TCPClient.ino
@@ -16,34 +16,180 @@
 */
 
 #include <GS2200Hal.h>
-#include "AppFunc.h"
-
+#include <GS2200AtCmd.h>
+#include <TelitWiFi.h>
+#include "config.h"
 
 #define  CONSOLE_BAUDRATE  115200
+/*-------------------------------------------------------------------------*
+ * Globals:
+ *-------------------------------------------------------------------------*/
+char TCP_Data[] = "GS2200 TCP Client Data Transfer Test.";
 
+extern uint8_t ESCBuffer[];
+extern uint32_t ESCBufferCnt;
+TelitWiFi gs2200;
+TWIFI_Params gsparams;
+/*-------------------------------------------------------------------------*
+ * Function ProtoTypes:
+ *-------------------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------------------*
+ * led_onoff
+ *---------------------------------------------------------------------------*/
+static void led_onoff(int num, bool stat)
+{
+	switch (num) {
+	case 0:
+		digitalWrite(LED0, stat);
+		break;
+
+	case 1:
+		digitalWrite(LED1, stat);
+		break;
+
+	case 2:
+		digitalWrite(LED2, stat);
+		break;
+
+	case 3:
+		digitalWrite(LED3, stat);
+		break;
+	}
+
+}
+
+/*---------------------------------------------------------------------------*
+ * led_effect
+ *---------------------------------------------------------------------------*
+ * Description: See this effect....
+ *---------------------------------------------------------------------------*/
+static void led_effect(void)
+{
+	static int cur=0, next;
+	int i;
+	static bool direction=true; // which way to go
+	
+
+	for (i=-1; i<5; i++) {
+		if (i==cur) {
+			led_onoff(i, true);
+			if (direction)
+				led_onoff(i-1, false);
+			else
+				led_onoff(i+1, false);
+		}
+	}
+
+	if (direction) { // 0 -> 1 -> 2 -> 3
+		if (++cur > 4)
+			direction = false;
+	}
+	else {
+		if (--cur < -1)
+			direction = true;
+	}
+		
+}
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
 	/* initialize digital pin of LEDs as an output. */
 	pinMode(LED0, OUTPUT);
 	pinMode(LED1, OUTPUT);
 	pinMode(LED2, OUTPUT);
 	pinMode(LED3, OUTPUT);
-	
-	digitalWrite( LED0, LOW );   // turn the LED off (LOW is the voltage level)
-	Serial.begin( CONSOLE_BAUDRATE ); // talk to PC
 
+	digitalWrite(LED0, LOW);   // turn the LED off (LOW is the voltage level)
+	Serial.begin(CONSOLE_BAUDRATE); // talk to PC
+
+	/* Initialize AT Command Library Buffer */
+	AtCmd_Init();
 	/* Initialize SPI access of GS2200 */
-	Init_GS2200_SPI();
+	Init_GS2200_SPI_type(iS110B_TypeC);
+	/* Initialize AT Command Library Buffer */
+	gsparams.mode = ATCMD_MODE_STATION;
+	gsparams.psave = ATCMD_PSAVE_DEFAULT;
+	if (gs2200.begin(gsparams)) {
+		ConsoleLog("GS2200 Initilization Fails");
+		while(1);
+	}
 
-	digitalWrite( LED0, HIGH ); // turn on LED
+	/* GS2200 Association to AP */
+	if (gs2200.activate_station(AP_SSID, PASSPHRASE)) {
+		ConsoleLog("Association Fails");
+		while(1);
+	}
+	digitalWrite(LED0, HIGH); // turn on LED
 
 }
 
 // the loop function runs over and over again forever
 void loop() {
+	ATCMD_RESP_E resp;
+	char server_cid = 0;
+	bool served = false;
+	ATCMD_NetworkStatus networkStatus;
+	uint32_t timer=0;
 
-	App_TCPClient_Test(); 	/* Never return */
+	while (1) {
+		if (!served) {
+			resp = ATCMD_RESP_UNMATCH;
+			// Start a TCP client
+			ConsoleLog("Start TCP Client");
+			resp = AtCmd_NCTCP((char *)TCPSRVR_IP, (char *)TCPSRVR_PORT, &server_cid);
+			if (resp != ATCMD_RESP_OK) {
+				ConsoleLog("No Connect!");
+				delay(2000);
+				continue;
+			}
+			if (server_cid == ATCMD_INVALID_CID) {
+				ConsoleLog("No CID!");
+				delay(2000);
+				continue;
+			}
+			
+			do {
+				resp = AtCmd_NSTAT(&networkStatus);
+			} while (ATCMD_RESP_OK != resp);
+			
+			ConsoleLog("Connected");
+			ConsolePrintf("IP: %d.%d.%d.%d\r\n", 
+				      networkStatus.addr.ipv4[0], networkStatus.addr.ipv4[1], networkStatus.addr.ipv4[2], networkStatus.addr.ipv4[3]);
+
+			served = true;
+		}
+		else {
+			ConsoleLog("Start to send TCP Data");
+			// Prepare for the next chunck of incoming data
+			WiFi_InitESCBuffer();
+
+			// Start the infinite loop to send the data
+			while (1) {
+				if (ATCMD_RESP_OK != AtCmd_SendBulkData(server_cid, TCP_Data, strlen(TCP_Data))) {
+					// Data is not sent, we need to re-send the data
+					delay(10);
+				}
+				
+				while (Get_GPIO37Status()) {
+					resp = AtCmd_RecvResponse();
+
+					if (ATCMD_RESP_BULK_DATA_RX == resp) {
+						if (Check_CID(server_cid)) {
+							ConsolePrintf("Receive %d byte:%s \r\n", ESCBufferCnt-1, ESCBuffer+1);
+						}
+						
+						WiFi_InitESCBuffer();
+					}
+				}
+
+				if (msDelta(timer) > 100) {
+					timer = millis();
+					led_effect();
+				}
+			}
+		}
+	}
 
 }
