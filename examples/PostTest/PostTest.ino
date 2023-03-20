0cd1e7a3ce396f5cdbea6b667043340bddc47e83
shixuejun-neusoft <shixuejun@neusoft.cojp>
2023-03-20 15:49:57 +0900

    fix PassThrough and PostTest ino.

--- examples/PostTest/PostTest.ino
+++ examples/PostTest/PostTest.ino
@@ -15,8 +15,7 @@
  *  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
-#include <GS2200Hal.h>
-#include <GS2200AtCmd.h>
+#include <HttpGs2200.h>
 #include <TelitWiFi.h>
 #include "config.h"
 
@@ -24,20 +23,17 @@
 #define  CONSOLE_BAUDRATE  115200
 #define  SEND_SIZE  100000
 
-
-extern uint8_t  *RespBuffer[];
-extern int   RespBuffer_Index;
-extern uint8_t ESCBuffer[];
-extern uint32_t ESCBufferCnt;
-
-
-char server_cid = 0;
-char httpsrvr_ip[16];
 char sendData[SEND_SIZE+1];
-
 TelitWiFi gs2200;
 TWIFI_Params gsparams;
+HttpGs2200 theHttpGs2200(&gs2200);
+HTTPGS2200_HostParams hostParams;
 
+const uint16_t RECEIVE_PACKET_SIZE = 1500;
+uint8_t Receive_Data[RECEIVE_PACKET_SIZE] = {0};
+bool result = false;
+int size;
+char size_string[10];
 
 void parse_httpresponse(char *message)
 {
@@ -57,12 +53,12 @@ void setup() {
 	Serial.begin( CONSOLE_BAUDRATE ); // talk to PC
 
 	/* Initialize SPI access of GS2200 */
-	Init_GS2200_SPI();
+	Init_GS2200_SPI_type(iS110B_TypeC);
 
 	/* Initialize AT Command Library Buffer */
 	gsparams.mode = ATCMD_MODE_STATION;
 	gsparams.psave = ATCMD_PSAVE_DEFAULT;
-	if( gs2200.begin( gsparams ) ){
+	if (gs2200.begin(gsparams)) {
 		ConsoleLog( "GS2200 Initilization Fails" );
 		while(1);
 	}
@@ -73,88 +69,45 @@ void setup() {
 		while(1);
 	}
 
-	digitalWrite( LED0, HIGH ); // turn on LED
+  hostParams.host = (char *)HTTP_SRVR_IP;
+  hostParams.port = (char *)HTTP_PORT;
+  theHttpGs2200.begin(&hostParams);
 
+  ConsoleLog("Start HTTP Client");
+
+  /* Set HTTP Headers */
+  theHttpGs2200.config(HTTP_HEADER_AUTHORIZATION, "Basic dGVzdDp0ZXN0MTIz");
+  theHttpGs2200.config(HTTP_HEADER_CONTENT_TYPE, "application/x-www-form-urlencoded");
+  theHttpGs2200.config(HTTP_HEADER_HOST, HTTP_SRVR_IP);
+
+  /* Prepare for the next chunck of incoming data */
+  WiFi_InitESCBuffer();
+
+  size = SEND_SIZE;
+  memset( sendData, '0', SEND_SIZE );
+  sendData[SEND_SIZE] = 0;
+
+	digitalWrite( LED0, HIGH ); // turn on LED
 }
 
 
 // the loop function runs over and over again forever
 void loop() {
+  /* Content-Length should be set BEFORE sending the data */
+  snprintf(size_string, sizeof(size_string), "%d", size);
+  theHttpGs2200.config(HTTP_HEADER_CONTENT_LENGTH, size_string);
 
-	ATCMD_RESP_E resp;
-	int count;
-	bool httpresponse=false;
-	uint32_t start;
-	uint32_t size;
-	char size_string[10];
-	
-	ConsoleLog( "Start HTTP Client");
+  result = theHttpGs2200.post(HTTP_POST_PATH, sendData);
 
-	/* Set HTTP Headers */
-	AtCmd_HTTPCONF( HTTP_HEADER_AUTHORIZATION, "Basic dGVzdDp0ZXN0MTIz" );
-	AtCmd_HTTPCONF( HTTP_HEADER_CONTENT_TYPE, "application/x-www-form-urlencoded" );
-	AtCmd_HTTPCONF( HTTP_HEADER_HOST, HTTP_SRVR_IP );
+  /* Need to receive the HTTP response */
+  if (0 < theHttpGs2200.receive(Receive_Data, RECEIVE_PACKET_SIZE)) {
+      parse_httpresponse( (char *)(Receive_Data) );
+  } else {
+    printf("theHttpGs2200.receive err.\n");
+  }
 
-	/* Prepare for the next chunck of incoming data */
-	WiFi_InitESCBuffer();
-	count = 0;
-
-	size = SEND_SIZE;
-	memset( sendData, '0', SEND_SIZE );
-	sendData[SEND_SIZE] = 0;
-	ConsoleLog( "POST Start" );
-
-	do {
-		resp = AtCmd_HTTPOPEN( &server_cid, HTTP_SRVR_IP, HTTP_PORT );
-	} while (ATCMD_RESP_OK != resp);
-	
-	ConsoleLog( "Socket Opened" );
-		
-	while( 1 ){
-		/* Content-Length should be set BEFORE sending the data */
-		sprintf( size_string, "%d", size );
-		do {
-			resp = AtCmd_HTTPCONF( HTTP_HEADER_CONTENT_LENGTH, size_string );
-		} while (ATCMD_RESP_OK != resp);
-		
-		do {
-			resp = AtCmd_HTTPSEND( server_cid, HTTP_METHOD_POST, 10, "/postData", sendData, size );
-		} while (ATCMD_RESP_OK != resp);
-		
-		/* Need to receive the HTTP response */
-		while( 1 ){
-			if( Get_GPIO37Status() ){
-				resp = AtCmd_RecvResponse();
-				
-				if( ATCMD_RESP_BULK_DATA_RX == resp ){
-					if( Check_CID( server_cid ) ){
-						parse_httpresponse( (char *)(ESCBuffer+1) );
-					}
-				}
-				WiFi_InitESCBuffer();
-				break;
-			}
-		}
-		
-		start = millis();
-		while(1){
-			if( Get_GPIO37Status() ){
-				resp = AtCmd_RecvResponse();
-				if( ATCMD_RESP_OK == resp ){
-					// AT+HTTPSEND command is done
-					break;
-				}
-			}
-			if( msDelta(start)>20000 ) // Timeout
-				break;
-		}
-		
-		delay( 1000 );
-
-	}
-
-	do {
-		resp = AtCmd_HTTPCLOSE( server_cid );
-	} while( (ATCMD_RESP_OK != resp) && (ATCMD_RESP_INVALID_CID != resp) );
-	ConsoleLog( "Socket Closed" );
+  /* Need to receive the HTTP response */
+  /* Timeout for 2000ms*/
+  result = theHttpGs2200.receive(2000);
+  result = theHttpGs2200.end();
 }
